1f7c4c057fdeab688baaf9aa7b450e7638e9daa3
Lixen <78407412+LikSunRice@users.noreply.github.com>
2026-01-27 22:47:35 +1100

    Revert "specify MemeMaker.tsx"
    This reverts commit e5d7558ce24c2cd0ab22167f939adace8877f6e6.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Connections from './projects/connections/Connections';
-<<<<<<< Updated upstream
-=======
-import MemeMaker from './projects/memeMaker/MemeMaker.tsx';
->>>>>>> Stashed changes
 
 const App: React.FC = () => {
   return (
